fdc05b11cccf6fc3bb8ab2b0d0e9591ff21d0460
Carlos Roman <carlos@cliche-corp.co.uk>
2021-03-02 10:31:48 +0000

    feat: Added ability to render triangles

---
+++ go/internal/object/triangle.go
@@ -0,0 +1,65 @@
+package object
+
+import (
+	"fmt"
+	"math"
+
+	"github.com/carlosroman/aun-otra-ray-trace/go/internal/ray"
+)
+
+type triangle struct {
+	obj
+	p1, p2, p3 ray.Vector
+	e1, e2     ray.Vector
+	normal     ray.Vector
+}
+
+func NewTriangle(p1, p2, p3 ray.Vector) Object {
+	t := triangle{
+		p1: p1,
+		p2: p2,
+		p3: p3,
+		e1: p2.Subtract(p1),
+		e2: p3.Subtract(p1),
+	}
+	t.normal = ray.Cross(t.e2, t.e1).Normalize()
+
+	_ = t.SetTransform(ray.DefaultIdentityMatrix())
+	t.SetMaterial(DefaultMaterial())
+	return &t
+}
+
+func (t triangle) String() string {
+	return fmt.Sprintf("e1: %v, e2: %v, normal: %v", t.e1, t.e2, t.normal)
+}
+
+func (t triangle) LocalNormalAt(_ ray.Vector) ray.Vector {
+	return t.normal
+}
+
+func (t triangle) LocalIntersect(r ray.Ray) Intersections {
+
+	dirCrossE2 := ray.Cross(r.Direction(), t.e2)
+	det := ray.Dot(t.e1, dirCrossE2)
+	if math.Abs(det) < epsilon {
+		return nil
+	}
+
+	f := 1.0 / det
+	p1ToOrig := r.Origin().Subtract(t.p1)
+	u := f * ray.Dot(p1ToOrig, dirCrossE2)
+	if u < 0 || u > 1 {
+		return nil
+	}
+
+	origCrossE1 := ray.Cross(p1ToOrig, t.e1)
+	v := f * ray.Dot(r.Direction(), origCrossE1)
+	if v < 0 || (u+v) > 1 {
+		return nil
+	}
+
+	return Intersections{Intersection{
+		T:   f * ray.Dot(t.e2, origCrossE1),
+		Obj: &t,
+	}}
+}
---
+++ go/internal/object/triangle_test.go
@@ -0,0 +1,106 @@
+package object_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/carlosroman/aun-otra-ray-trace/go/internal/object"
+	"github.com/carlosroman/aun-otra-ray-trace/go/internal/ray"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestNewTriangle(t *testing.T) {
+	tr := object.NewTriangle(
+		ray.NewPoint(0, 1, 0), ray.NewPoint(-1, 0, 0), ray.NewPoint(1, 0, 0))
+	assert.Equal(t, "e1: -1,-1,0,0, e2: 1,-1,0,0, normal: 0,-0,-1,0", fmt.Sprintf("%v", tr))
+}
+
+func TestTriangle_LocalNormalAt(t *testing.T) {
+
+	tr := object.NewTriangle(
+		ray.NewPoint(0, 1, 0), ray.NewPoint(-1, 0, 0), ray.NewPoint(1, 0, 0))
+
+	en := ray.Cross(
+		ray.NewPoint(1, 0, 0).Subtract(ray.NewPoint(0, 1, 0)),
+		ray.NewPoint(-1, 0, 0).Subtract(ray.NewPoint(0, 1, 0))).
+		Normalize()
+
+	testCases := []struct {
+		name           string
+		point          ray.Vector
+		expectedNormal ray.Vector
+	}{
+		{
+			name:           "one",
+			point:          ray.NewPoint(0, 0.5, 0),
+			expectedNormal: en,
+		},
+		{
+			name:           "two",
+			point:          ray.NewPoint(-0.5, 0.75, 0),
+			expectedNormal: en,
+		},
+		{
+			name:           "three",
+			point:          ray.NewPoint(0.5, 0.25, 0),
+			expectedNormal: en,
+		},
+	}
+
+	for _, tt := range testCases {
+		t.Run(tt.name, func(t *testing.T) {
+			actual := tr.LocalNormalAt(tt.point)
+			assertVec(t, tt.expectedNormal, actual)
+		})
+	}
+}
+
+func TestTriangle_LocalIntersect(t *testing.T) {
+	tr := object.NewTriangle(
+		ray.NewPoint(0, 1, 0), ray.NewPoint(-1, 0, 0), ray.NewPoint(1, 0, 0))
+
+	testCases := []struct {
+		name       string
+		expectedXs object.Intersections
+		ray        ray.Ray
+	}{
+		{
+			name:       "Intersecting a ray parallel to the triangle",
+			expectedXs: nil,
+			ray:        ray.NewRayAt(ray.NewPoint(0, -1, -2), ray.NewVec(0, 1, 0)),
+		},
+		{
+			name:       " A ray misses the p1-p3 edge",
+			expectedXs: nil,
+			ray:        ray.NewRayAt(ray.NewPoint(1, 1, -2), ray.NewVec(0, 0, 1)),
+		},
+		{
+			name:       "A ray misses the p1-p2 edge",
+			expectedXs: nil,
+			ray:        ray.NewRayAt(ray.NewPoint(-1, -1, -2), ray.NewVec(0, 0, 1)),
+		},
+		{
+			name:       "A ray misses the p2-p3 edge",
+			expectedXs: nil,
+			ray:        ray.NewRayAt(ray.NewPoint(0, -1, -2), ray.NewVec(0, 0, 1)),
+		},
+		{
+			name: "A ray strikes a triangle",
+			expectedXs: object.Intersections{
+				{T: 2, Obj: tr}},
+			ray: ray.NewRayAt(ray.NewPoint(0, 0.5, -2), ray.NewVec(0, 0, 1)),
+		},
+	}
+
+	for _, tt := range testCases {
+		t.Run(tt.name, func(t *testing.T) {
+			actual := tr.LocalIntersect(tt.ray)
+			require.Len(t, actual, len(tt.expectedXs))
+			for i := range tt.expectedXs {
+				assert.Equal(t, tt.expectedXs[i].T, actual[i].T, fmt.Sprintf("Checking xs[%v].T", i))
+				assert.Equal(t, tt.expectedXs[i].Obj, actual[i].Obj, fmt.Sprintf("Checking xs[%v].Obj", i))
+			}
+		})
+	}
+}
