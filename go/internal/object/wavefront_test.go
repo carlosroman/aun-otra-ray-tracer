3a05722561777b3e5caa4a95dc1d4a7eec45d009
Carlos Roman <carlos@cliche-corp.co.uk>
2021-10-22 16:27:43 +0100

    feat: Renamed obj to wavObj wavefront_test.go

--- go/internal/object/wavefront_test.go
+++ go/internal/object/wavefront_test.go
@@ -25,12 +25,12 @@ func TestNewWavefrontObj(t *testing.T) {
 	testCases := []struct {
 		name        string
 		fileContent string
-		assertFunc  func(t *testing.T, obj object.WavefrontObj)
+		assertFunc  func(t *testing.T, wavObj object.WavefrontObj)
 	}{
 		{
 			name: "Ignoring unrecognized lines",
-			assertFunc: func(t *testing.T, obj object.WavefrontObj) {
-				assert.Empty(t, obj.Vertices)
+			assertFunc: func(t *testing.T, wavObj object.WavefrontObj) {
+				assert.Empty(t, wavObj.Vertices)
 			},
 			fileContent: `There was a young lady named Bright
 who traveled much faster than light.
@@ -41,13 +41,13 @@ and came back the previous nigh
 		},
 		{
 			name: "Vertex records",
-			assertFunc: func(t *testing.T, obj object.WavefrontObj) {
-				require.Len(t, obj.Vertices, 4)
+			assertFunc: func(t *testing.T, wavObj object.WavefrontObj) {
+				require.Len(t, wavObj.Vertices, 4)
 
-				assertVec(t, obj.Vertices[0], ray.NewPoint(-1, 1, 0))
-				assertVec(t, obj.Vertices[1], ray.NewPoint(-1, 0.5, 0))
-				assertVec(t, obj.Vertices[2], ray.NewPoint(1, 0, 0))
-				assertVec(t, obj.Vertices[3], ray.NewPoint(1, 1, 0))
+				assertVec(t, wavObj.Vertices[0], ray.NewPoint(-1, 1, 0))
+				assertVec(t, wavObj.Vertices[1], ray.NewPoint(-1, 0.5, 0))
+				assertVec(t, wavObj.Vertices[2], ray.NewPoint(1, 0, 0))
+				assertVec(t, wavObj.Vertices[3], ray.NewPoint(1, 1, 0))
 			},
 			fileContent: `v -1 1 0
 v -1.0000 0.5000 0.0000
@@ -57,15 +57,15 @@ v 1 1 0
 		},
 		{
 			name: "Parsing triangle faces",
-			assertFunc: func(t *testing.T, obj object.WavefrontObj) {
-				require.Len(t, obj.Group.Children, 2)
+			assertFunc: func(t *testing.T, wavObj object.WavefrontObj) {
+				require.Len(t, wavObj.Group.Children, 2)
 
-				t1 := object.NewTriangle(obj.Vertices[0], obj.Vertices[1], obj.Vertices[2])
-				t1.SetParent(&obj.Group)
-				assert.Equal(t, t1, obj.Group.Children[0])
-				t2 := object.NewTriangle(obj.Vertices[0], obj.Vertices[2], obj.Vertices[3])
-				t2.SetParent(&obj.Group)
-				assert.Equal(t, t2, obj.Group.Children[1])
+				t1 := object.NewTriangle(wavObj.Vertices[0], wavObj.Vertices[1], wavObj.Vertices[2])
+				t1.SetParent(&wavObj.Group)
+				assert.Equal(t, t1, wavObj.Group.Children[0])
+				t2 := object.NewTriangle(wavObj.Vertices[0], wavObj.Vertices[2], wavObj.Vertices[3])
+				t2.SetParent(&wavObj.Group)
+				assert.Equal(t, t2, wavObj.Group.Children[1])
 			},
 			fileContent: `v -1 1 0
 v -1 0 0
@@ -78,15 +78,15 @@ f 1 3 4
 		},
 		{
 			name: "Parsing triangle faces with texture coordinate indices and normal indices",
-			assertFunc: func(t *testing.T, obj object.WavefrontObj) {
-				require.Len(t, obj.Group.Children, 2)
+			assertFunc: func(t *testing.T, wavObj object.WavefrontObj) {
+				require.Len(t, wavObj.Group.Children, 2)
 
-				t1 := object.NewTriangle(obj.Vertices[0], obj.Vertices[1], obj.Vertices[2])
-				t1.SetParent(&obj.Group)
-				assert.Equal(t, t1, obj.Group.Children[0])
-				t2 := object.NewTriangle(obj.Vertices[0], obj.Vertices[2], obj.Vertices[3])
-				t2.SetParent(&obj.Group)
-				assert.Equal(t, t2, obj.Group.Children[1])
+				t1 := object.NewTriangle(wavObj.Vertices[0], wavObj.Vertices[1], wavObj.Vertices[2])
+				t1.SetParent(&wavObj.Group)
+				assert.Equal(t, t1, wavObj.Group.Children[0])
+				t2 := object.NewTriangle(wavObj.Vertices[0], wavObj.Vertices[2], wavObj.Vertices[3])
+				t2.SetParent(&wavObj.Group)
+				assert.Equal(t, t2, wavObj.Group.Children[1])
 			},
 			fileContent: `v -1 1 0
 v -1 0 0
@@ -99,15 +99,15 @@ f 1//1 3//3 4//4
 		},
 		{
 			name: "Triangulating square",
-			assertFunc: func(t *testing.T, obj object.WavefrontObj) {
-				require.Len(t, obj.Group.Children, 2)
+			assertFunc: func(t *testing.T, wavObj object.WavefrontObj) {
+				require.Len(t, wavObj.Group.Children, 2)
 
-				t1 := object.NewTriangle(obj.Vertices[1-1], obj.Vertices[2-1], obj.Vertices[3-1])
-				t1.SetParent(&obj.Group)
-				assert.Equal(t, t1, obj.Group.Children[0], "t1")
-				t2 := object.NewTriangle(obj.Vertices[1-1], obj.Vertices[3-1], obj.Vertices[4-1])
-				t2.SetParent(&obj.Group)
-				assert.Equal(t, t2, obj.Group.Children[1], "t2")
+				t1 := object.NewTriangle(wavObj.Vertices[1-1], wavObj.Vertices[2-1], wavObj.Vertices[3-1])
+				t1.SetParent(&wavObj.Group)
+				assert.Equal(t, t1, wavObj.Group.Children[0], "t1")
+				t2 := object.NewTriangle(wavObj.Vertices[1-1], wavObj.Vertices[3-1], wavObj.Vertices[4-1])
+				t2.SetParent(&wavObj.Group)
+				assert.Equal(t, t2, wavObj.Group.Children[1], "t2")
 			},
 			fileContent: `v -1 1 0
 v -1 0 0
@@ -120,18 +120,18 @@ f 1 2 3 4
 		},
 		{
 			name: "Triangulating polygons",
-			assertFunc: func(t *testing.T, obj object.WavefrontObj) {
-				require.Len(t, obj.Group.Children, 3)
+			assertFunc: func(t *testing.T, wavObj object.WavefrontObj) {
+				require.Len(t, wavObj.Group.Children, 3)
 
-				t1 := object.NewTriangle(obj.Vertices[1-1], obj.Vertices[2-1], obj.Vertices[3-1])
-				t1.SetParent(&obj.Group)
-				assert.Equal(t, t1, obj.Group.Children[0])
-				t2 := object.NewTriangle(obj.Vertices[1-1], obj.Vertices[3-1], obj.Vertices[4-1])
-				t2.SetParent(&obj.Group)
-				assert.Equal(t, t2, obj.Group.Children[1])
-				t3 := object.NewTriangle(obj.Vertices[1-1], obj.Vertices[4-1], obj.Vertices[5-1])
-				t3.SetParent(&obj.Group)
-				assert.Equal(t, t3, obj.Group.Children[2])
+				t1 := object.NewTriangle(wavObj.Vertices[1-1], wavObj.Vertices[2-1], wavObj.Vertices[3-1])
+				t1.SetParent(&wavObj.Group)
+				assert.Equal(t, t1, wavObj.Group.Children[0])
+				t2 := object.NewTriangle(wavObj.Vertices[1-1], wavObj.Vertices[3-1], wavObj.Vertices[4-1])
+				t2.SetParent(&wavObj.Group)
+				assert.Equal(t, t2, wavObj.Group.Children[1])
+				t3 := object.NewTriangle(wavObj.Vertices[1-1], wavObj.Vertices[4-1], wavObj.Vertices[5-1])
+				t3.SetParent(&wavObj.Group)
+				assert.Equal(t, t3, wavObj.Group.Children[2])
 			},
 			fileContent: `v -1 1 0
 v -1 0 0
@@ -144,20 +144,20 @@ f 1 2 3 4 5
 		},
 		{
 			name: "Faces with normals",
-			assertFunc: func(t *testing.T, obj object.WavefrontObj) {
-				require.Len(t, obj.Group.Children, 2)
-				require.Len(t, obj.Normals, 3)
+			assertFunc: func(t *testing.T, wavObj object.WavefrontObj) {
+				require.Len(t, wavObj.Group.Children, 2)
+				require.Len(t, wavObj.Normals, 3)
 
-				t1 := object.NewSmoothTriangle(obj.Vertices[1-1], obj.Vertices[2-1], obj.Vertices[3-1],
-					obj.Normals[3-1], obj.Normals[1-1], obj.Normals[2-1],
+				t1 := object.NewSmoothTriangle(wavObj.Vertices[1-1], wavObj.Vertices[2-1], wavObj.Vertices[3-1],
+					wavObj.Normals[3-1], wavObj.Normals[1-1], wavObj.Normals[2-1],
 				)
-				t1.SetParent(&obj.Group)
-				assert.Equal(t, t1, obj.Group.Children[0])
-				t2 := object.NewSmoothTriangle(obj.Vertices[3-1], obj.Vertices[2-1], obj.Vertices[1-1],
-					obj.Normals[2-1], obj.Normals[1-1], obj.Normals[3-1],
+				t1.SetParent(&wavObj.Group)
+				assert.Equal(t, t1, wavObj.Group.Children[0])
+				t2 := object.NewSmoothTriangle(wavObj.Vertices[3-1], wavObj.Vertices[2-1], wavObj.Vertices[1-1],
+					wavObj.Normals[2-1], wavObj.Normals[1-1], wavObj.Normals[3-1],
 				)
-				t2.SetParent(&obj.Group)
-				assert.Equal(t, t2, obj.Group.Children[1])
+				t2.SetParent(&wavObj.Group)
+				assert.Equal(t, t2, wavObj.Group.Children[1])
 
 			},
 			fileContent: `v 0 1 0
@@ -174,18 +174,18 @@ f 3/0/2 2/102/1 1/14/3
 		},
 		{
 			name: "Triangles in groups",
-			assertFunc: func(t *testing.T, obj object.WavefrontObj) {
-				require.Len(t, obj.Group.Children, 2)
+			assertFunc: func(t *testing.T, wavObj object.WavefrontObj) {
+				require.Len(t, wavObj.Group.Children, 2)
 
-				t1 := object.NewTriangle(obj.Vertices[1-1], obj.Vertices[2-1], obj.Vertices[3-1])
-				g1, ok := obj.Group.Children[0].(*object.Group)
+				t1 := object.NewTriangle(wavObj.Vertices[1-1], wavObj.Vertices[2-1], wavObj.Vertices[3-1])
+				g1, ok := wavObj.Group.Children[0].(*object.Group)
 				require.True(t, ok, "Can not assign G1 as object.Group")
 				t1.SetParent(g1) // Setting p
 				require.Len(t, g1.Children, 1)
 				assert.Equal(t, t1, g1.Children[0], "G1 child")
 
-				t2 := object.NewTriangle(obj.Vertices[1-1], obj.Vertices[3-1], obj.Vertices[4-1])
-				g2, ok := obj.Group.Children[1].(*object.Group)
+				t2 := object.NewTriangle(wavObj.Vertices[1-1], wavObj.Vertices[3-1], wavObj.Vertices[4-1])
+				g2, ok := wavObj.Group.Children[1].(*object.Group)
 				require.True(t, ok, "Can not assign G2 as object.Group")
 				t2.SetParent(g2)
 				require.Len(t, g2.Children, 1)
@@ -224,11 +224,11 @@ v 1 1 0
 f 1 2 3`
 
 	bufferString := bytes.NewBufferString(content)
-	obj, err := object.NewWavefrontObj(bufferString)
+	wavObj, err := object.NewWavefrontObj(bufferString)
 	require.NoError(t, err)
-	actual := obj.Object()
+	actual := wavObj.Object()
 
-	t1 := object.NewTriangle(obj.Vertices[1-1], obj.Vertices[2-1], obj.Vertices[3-1])
+	t1 := object.NewTriangle(wavObj.Vertices[1-1], wavObj.Vertices[2-1], wavObj.Vertices[3-1])
 	g1, ok := actual.(*object.Group)
 	require.True(t, ok, "Can not assign G1 as object.Group")
 	t1.SetParent(g1) // Setting p
